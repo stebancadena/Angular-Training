3193676065028555d29a6efbc314e13c48fe9123
Steban Cadena <Steban.Cadena@jalasoft.com>
2022-04-29 08:51:51 -0500

    Initial routing of pages and guards for some routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,34 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {
+        path: 'products/:id',
+        canActivate: [ProductDetailGuard], 
+        component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    ])
   ],
   bootstrap: [AppComponent]
 })
